578a42af4ec2dc3b80174c03239d9fc46f2f191a
Perebonilado <richad.eradiri@cicod.com>
2023-03-24 14:06:50 +0100

    middleware fix

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { ACCESS_TOKEN } from "./constants";
 
-export function middleware(req: NextRequest, res: NextResponse) {
+export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
   const storedCookie = req.cookies.get(ACCESS_TOKEN);
 
