08da3bd0035c543e95845ae51736615a902e8a73
perebonilado <reradiri@eminenttechnology.com>
2023-05-01 09:10:56 +0100

    sos typed

---
+++ models/Sos.ts
@@ -0,0 +1,71 @@
+export interface SosTableData {
+  id: string;
+  origin: string;
+  destination: string;
+  rider: string;
+  driver: string;
+  carModel: string;
+  plateNumber: string;
+  status: string;
+  raisedBy: string;
+  reason: string;
+}
+
+export interface Sos {
+  _id: string;
+  trip: {
+    start_address: {
+      country: string;
+      state: string;
+      city: string;
+    };
+    end_address: {
+      country: string;
+      state: string;
+      city: string;
+    };
+    _id: string;
+    user: {
+      _id: string;
+      full_name: string;
+    };
+    driver: {
+      _id: string;
+      full_name: string;
+    };
+    car: {
+      _id: string;
+      brand_name: string;
+      model: string;
+      year: string;
+      color: string;
+      plate_number: string;
+    };
+    id: string;
+  };
+  time_of_action: Date;
+  status: string;
+  reason: string;
+  createdAt: string;
+  id: string;
+}
+
+export interface GetAllSOSResponse {
+  status: string;
+  data: {
+    data: Sos[];
+    pagination: {
+      pageSize: number;
+      totalCount: number;
+      pageCount: number;
+      currentPage: number;
+      hasNext: boolean;
+    };
+  };
+  message: string;
+}
+
+export interface MappedSosResponse {
+    data: SosTableData[];
+    totalCount: number;
+}
