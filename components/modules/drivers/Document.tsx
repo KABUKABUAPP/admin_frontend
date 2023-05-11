37396c5ce1aa8e40b013046d6366fb1b7e51aaf3
perebonilado <reradiri@eminenttechnology.com>
2023-05-11 12:57:39 +0100

    image fix

--- components/modules/drivers/Document.tsx
+++ components/modules/drivers/Document.tsx
@@ -12,13 +12,15 @@ const Document: FC<Props> = ({ docId, docImage, title }) => {
   return (
     <div className="flex flex-col gap-2">
       {docImage && (
-        <div className="relative w-full max-w-[400px] h-[200px]">
-          <Image
-            layout="fill"
-            src={docImage}
-            style={{ objectFit: "cover" }}
-            alt="uploaded document"
-          />
+        <div className="relative ">
+          <div className="w-full h-[150px] max-w-[400px] relative">
+            <Image
+              layout="fill"
+              src={docImage}
+              alt="uploaded document"
+              objectFit='cover'
+            />
+          </div>
         </div>
       )}
       <p className="text-lg font-semibold">{title}</p>
