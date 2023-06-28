2afb7c273b5e85000579510ad3512ce62ddc7764
perebonilado <reradiri@eminenttechnology.com>
2023-06-28 21:14:03 +0100

    phone number character length

--- components/common/DeleteableImagesCard.tsx
+++ components/common/DeleteableImagesCard.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useRef } from "react";
 
 import Card from "@/components/common/Card";
 import Button from "@/components/ui/Button/Button";
@@ -10,6 +10,9 @@ interface Props {
 }
 
 const DeleteableImagesCard: FC<Props> = ({ images, title }) => {
+  
+  const fileInputRef = useRef<any>()
+
   return (
     <Card>
       <p className="text-lg font-semibold pb-2">{title || `Car Images`}</p>
@@ -19,7 +22,13 @@ const DeleteableImagesCard: FC<Props> = ({ images, title }) => {
         color="tetiary"
         size="small"
         className="!text-[#9A9A9A]"
+        onClick={()=>{
+          if(fileInputRef){
+            fileInputRef.current.click()
+          }
+        }}
       />
+      <input type="file" className="hidden" ref={fileInputRef} />
 
       <div className="pt-2 flex max-w-[300px] overflow-x-auto scrollbar-none gap-2 ">
         {images?.map((img, idx) => (
