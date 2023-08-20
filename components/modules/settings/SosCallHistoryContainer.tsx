dba96dbebb557a41d2061be4637edd62544ec464
perebonilado <reradiri@eminenttechnology.com>
2023-08-20 14:49:54 +0100

    sos contact list height fix

--- components/modules/settings/SosCallHistoryContainer.tsx
+++ components/modules/settings/SosCallHistoryContainer.tsx
@@ -11,7 +11,7 @@ interface Props {
 
 const SosCallHistoryContainer: FC<Props> = ({ data }) => {
   return (
-    <div className="flex flex-col gap-4 max-h-[300px] max-md:max-h-[800px] overflow-auto scrollbar-none">
+    <div className="flex flex-col gap-4 max-h-[600px] max-md:max-h-[1200px] overflow-auto scrollbar-none">
       {data.map((ch, idx) => {
         return <SosCallHistoryItem {...ch} key={idx} />;
       })}
