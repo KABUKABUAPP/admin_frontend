196d547df8ac59ef65f3ceb242e5673f506a8751
Reiz Janto <betswanky@gmail.com>
2023-11-14 10:11:09 +0100

    rebuild staff view

--- components/modules/staff/ActivityLogItem.tsx
+++ components/modules/staff/ActivityLogItem.tsx
@@ -1,15 +1,16 @@
 import React, { FC } from "react";
+import moment from 'moment';
 
 interface Props {
-  title: string;
-  date: string;
+  description: string;
+  createdAt: string;
 }
 
-const ActivityLogItem: FC<Props> = ({ title, date }) => {
+const ActivityLogItem: FC<Props> = ({ description, createdAt }) => {
   return (
     <div className="flex justify-between max-sm:flex-col-reverse items-center max-sm:items-start gap-2 py-3 border-b border-b-[#9A9A9A]">
-      <p className="text-base font-semibold">{title}</p>
-      <p className="text-sm text-[#9A9A9A]">{date}</p>
+      <p className="text-base font-semibold">{description}</p>
+      <p className="text-sm text-[#9A9A9A]">{moment(createdAt).fromNow()}</p>
     </div>
   );
 };
