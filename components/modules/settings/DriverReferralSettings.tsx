6a90bea8303ee1aa93773527a5b7297152a39665
Reiz Janto <betswanky@gmail.com>
2023-11-16 21:03:25 +0100

    Driver withdrawal and referral settings edited

--- components/modules/settings/DriverReferralSettings.tsx
+++ components/modules/settings/DriverReferralSettings.tsx
@@ -46,7 +46,7 @@ const DriverReferralSettings: FC = () => {
         if (formik.values.no_of_trips.length > 0 && formik.values.amount_per_referral.length > 0) setShowSaveChanges(true);
         if (formik.values.no_of_trips.length === 0 || formik.values.amount_per_referral.length === 0) setShowSaveChanges(false)
     }, [formik.values])
-    
+
     useEffect(() => {
         if (isSuccess) toast.success('Withdrawal Settings Updated Successfully')
     }, [isSuccess])
@@ -59,69 +59,66 @@ const DriverReferralSettings: FC = () => {
         <>
             <h1 className="text-3xl font-bold">Driver Referral Settings</h1>
             <p className="mt-2 text-[#9A9A9A]"><small>Configure driver referrals</small></p>
-            {
-                settingsLoading ?
-                <Loader /> :
-                <FormikProvider value={formik}>
-                    <Form>
-                        <>
-                            <div className="flex flex-col gap-8 py-5">
-                                <div className="flex justify-between gap-3 max-sm:flex-col">
+            
+            <FormikProvider value={formik}>
+                <Form>
+                    <>
+                        <div className="flex flex-col gap-8 py-5">
+                            <div className="flex justify-between gap-3 max-sm:flex-col">
 
-                                    <div className="w-full">
-                                        <TextField
-                                            label="Trips to be completed"
-                                            placeholder="Trips to be completed"
-                                            {...formik.getFieldProps("no_of_trips")}
-                                            error={
-                                            formik.touched.no_of_trips ? formik.errors.no_of_trips : undefined
-                                            }
-                                        />
-                                        <div className="flex mt-2">
-                                            <p><InfoIcon /></p> 
-                                            <p className="ml-2 text-[#9A9A9A]"><small>How many trips does the driver need to complete before the driver recieves value</small></p>
-                                        </div>
-                                        
+                                <div className="w-full">
+                                    <TextField
+                                        label="Trips to be completed"
+                                        placeholder="Trips to be completed"
+                                        {...formik.getFieldProps("no_of_trips")}
+                                        error={
+                                        formik.touched.no_of_trips ? formik.errors.no_of_trips : undefined
+                                        }
+                                    />
+                                    <div className="flex mt-2">
+                                        <p><InfoIcon /></p> 
+                                        <p className="ml-2 text-[#9A9A9A]"><small>How many trips does the driver need to complete before the driver recieves value</small></p>
                                     </div>
-                                    <div className="w-full">
                                     
-                                        <TextField
-                                            label="Amount Per Referral"
-                                            placeholder="Amount Per Referral"
-                                            {...formik.getFieldProps("amount_per_referral")}
-                                            error={
-                                            formik.touched.amount_per_referral ? formik.errors.amount_per_referral : undefined
-                                            }
-                                        />
-                                        <div className="flex mt-2">
-                                            <p><InfoIcon /></p> 
-                                            <p className="ml-2 text-[#9A9A9A]"><small>How much does the driver get per referral</small></p>
-                                        </div>
-                                        
+                                </div>
+                                <div className="w-full">
+                                
+                                    <TextField
+                                        label="Amount Per Referral"
+                                        placeholder="Amount Per Referral"
+                                        {...formik.getFieldProps("amount_per_referral")}
+                                        error={
+                                        formik.touched.amount_per_referral ? formik.errors.amount_per_referral : undefined
+                                        }
+                                    />
+                                    <div className="flex mt-2">
+                                        <p><InfoIcon /></p> 
+                                        <p className="ml-2 text-[#9A9A9A]"><small>How much does the driver get per referral</small></p>
                                     </div>
+                                    
                                 </div>
                             </div>
-                            
-                            {
-                                showSaveChanges &&
-                                <div className="p-4 flex justify-end">
-                                    <Button
-                                        title="Save Changes"
-                                        className="!text-[16px] mt-6"
-                                        size="large"
-                                        type="submit"
-                                        loading={isLoading}
-                                        disabled={isLoading}
-                                        onClick={() => {
-                                        if (formik.isValid) formik.submitForm();
-                                        }}
-                                    />
-                                </div>
-                            }
-                        </>
-                    </Form>
-                </FormikProvider>
-            }
+                        </div>
+                        
+                        {
+                            showSaveChanges &&
+                            <div className="p-4 flex justify-end">
+                                <Button
+                                    title="Save Changes"
+                                    className="!text-[16px] mt-6"
+                                    size="large"
+                                    type="submit"
+                                    loading={isLoading}
+                                    disabled={isLoading}
+                                    onClick={() => {
+                                    if (formik.isValid) formik.submitForm();
+                                    }}
+                                />
+                            </div>
+                        }
+                    </>
+                </Form>
+            </FormikProvider>
         </>
     )
 }
--- components/modules/settings/DriverWithdrawalSettings.tsx
+++ components/modules/settings/DriverWithdrawalSettings.tsx
@@ -65,72 +65,68 @@ const DriverWithdrawalSettings: FC = () => {
         <>
             <h1 className="text-3xl font-bold">Driver Withdrawal Settings</h1>
             <p className="mt-2 text-[#9A9A9A]"><small>Configure driver withdrawals</small></p>
+            
+            <FormikProvider value={formik}>
+                <Form>
+                    <>
+                        <div className="flex flex-col gap-8 py-5">
+                            <div className="flex justify-between gap-3 max-sm:flex-col">
 
-            {
-                settingsLoading ?
-                <Loader /> :
-                <FormikProvider value={formik}>
-                    <Form>
-                        <>
-                            <div className="flex flex-col gap-8 py-5">
-                                <div className="flex justify-between gap-3 max-sm:flex-col">
-
-                                    <div className="w-full">
-                                        <SelectField
-                                            options={withdrawalTypes}
-                                            disabled={false}
-                                            label="Withrawal Type"
-                                            placeholder="Withdrawal Type"
-                                            className="w-full"
-                                            {...formik.getFieldProps("withdrawal_type")}
-                                            error={
-                                            formik.touched.withdrawal_type ? formik.errors.withdrawal_type : undefined
-                                            }
-                                        />
-                                        <div className="flex mt-2">
-                                            <p><InfoIcon /></p> 
-                                            <p className="ml-2 text-[#9A9A9A]"><small>How often should they withdraw</small></p>
-                                        </div>
+                                <div className="w-full">
+                                    <SelectField
+                                        options={withdrawalTypes}
+                                        disabled={false}
+                                        label="Withrawal Type"
+                                        placeholder="Withdrawal Type"
+                                        className="w-full"
+                                        {...formik.getFieldProps("withdrawal_type")}
+                                        error={
+                                        formik.touched.withdrawal_type ? formik.errors.withdrawal_type : undefined
+                                        }
+                                    />
+                                    <div className="flex mt-2">
+                                        <p><InfoIcon /></p> 
+                                        <p className="ml-2 text-[#9A9A9A]"><small>How often should they withdraw</small></p>
                                     </div>
-                                    <div className="w-full">
+                                </div>
+                                <div className="w-full">
+                                
+                                    <TextField
+                                        label="Withrawal Frequency"
+                                        placeholder="Withrawal Frequency"
+                                        {...formik.getFieldProps("withdrawal_frequency")}
+                                        error={
+                                        formik.touched.withdrawal_frequency ? formik.errors.withdrawal_frequency : undefined
+                                        }
+                                    />
+                                    <div className="flex mt-2">
+                                        <p><InfoIcon /></p> 
+                                        <p className="ml-2 text-[#9A9A9A]"><small>How many times during that period should they withdraw</small></p>
+                                    </div>
                                     
-                                        <TextField
-                                            label="Withrawal Frequency"
-                                            placeholder="Withrawal Frequency"
-                                            {...formik.getFieldProps("withdrawal_frequency")}
-                                            error={
-                                            formik.touched.withdrawal_frequency ? formik.errors.withdrawal_frequency : undefined
-                                            }
-                                        />
-                                        <div className="flex mt-2">
-                                            <p><InfoIcon /></p> 
-                                            <p className="ml-2 text-[#9A9A9A]"><small>How many times during that period should they withdraw</small></p>
-                                        </div>
-                                        
-                                    </div>
                                 </div>
                             </div>
-    
-                            {
-                                showSaveChanges &&
-                                <div className="p-4 flex justify-end">
-                                    <Button
-                                        title="Save Changes"
-                                        className="!text-[16px] mt-6"
-                                        size="large"
-                                        type="submit"
-                                        loading={isLoading}
-                                        disabled={isLoading}
-                                        onClick={() => {
-                                        if (formik.isValid) formik.submitForm();
-                                        }}
-                                    />
-                                </div>
-                            }
-                        </>
-                    </Form>
-                </FormikProvider>
-            }
+                        </div>
+
+                        {
+                            showSaveChanges &&
+                            <div className="p-4 flex justify-end">
+                                <Button
+                                    title="Save Changes"
+                                    className="!text-[16px] mt-6"
+                                    size="large"
+                                    type="submit"
+                                    loading={isLoading}
+                                    disabled={isLoading}
+                                    onClick={() => {
+                                    if (formik.isValid) formik.submitForm();
+                                    }}
+                                />
+                            </div>
+                        }
+                    </>
+                </Form>
+            </FormikProvider>
         </>
     )
 }
