25efcb414be983f9b6b3f3d922617c0de094d62e
Reiz Janto <betswanky@gmail.com>
2024-03-03 21:50:30 +0100

    Testing car owners route issue 4

--- api-services/carOwnersService.ts
+++ api-services/carOwnersService.ts
@@ -25,10 +25,9 @@ const baseQueryWithLogoutOnTokenExpiration: BaseQueryFn<
 > = async (args, api, extraOptions) => {
   let result = await baseQuery(args, api, extraOptions);
   if (result.error && result.error.status === 401) {
-    console.log(result.error)
-    /*logout(()=>{
+    logout(()=>{
       window.location.pathname = '/auth/login'
-    });*/
+    });
   }
   return result;
 };
