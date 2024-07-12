aaa326dcb474f9c593664199a5ace0d9a41c34b5
Reiz Janto <betswanky@gmail.com>
2024-07-12 17:51:10 +0100

    Today's online activity

--- pages/drivers/active/[id].tsx
+++ pages/drivers/active/[id].tsx
@@ -26,11 +26,15 @@ import { useModalContext } from "@/contexts/ModalContext";
 import { toast } from "react-toastify";
 import useUserPermissions from "@/hooks/useUserPermissions";
 import AppHead from "@/components/common/AppHead";
-import { capitalizeAllFirstLetters } from "@/utils";
+import { capitalizeAllFirstLetters, capitalizeFirstLetter } from "@/utils";
 import Card from "@/components/common/Card";
 import TransactionsIcon from "@/components/icons/TransactionsIcon";
 import FundDriverWallet from "@/components/modules/drivers/FundDriverWallet";
 import { useUserContext } from "@/contexts/UserContext";
+import TimesIconRed from "@/components/icons/TimesIconRed";
+import { useGetOnlineMonitorQuery } from "@/api-services/monitorService";
+import TextField from "@/components/ui/Input/TextField/TextField";
+import SearchIcon from "@/components/icons/SearchIcon";
 
 function timeAgo(timeString: any) {
   const currentDate = new Date();
@@ -62,10 +66,68 @@ function timeAgo(timeString: any) {
   }
 }
 
+function formatTime(dateString: any) {
+  const date = new Date(dateString);
+
+  if (isNaN(date.getTime())) {
+      throw new Error('Invalid date value');
+  }
+
+  let hours = date.getUTCHours();
+  const minutes = date.getUTCMinutes();
+  const ampm = hours >= 12 ? 'pm' : 'am';
+
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+
+  const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
+
+  return `${hours}:${formattedMinutes}${ampm}`;
+}
+
+function calculateHoursBetween(startTime: any, endTime: any) {
+  const startDate = new Date(startTime);
+  const endDate = new Date(endTime);
+
+  if (isNaN(startDate.getTime()) || isNaN(endDate.getTime())) {
+      throw new Error('Invalid date value');
+  }
+
+  const differenceInMilliseconds = endDate.getTime() - startDate.getTime();
+  const differenceInHours = differenceInMilliseconds / (1000 * 60 * 60);
+
+  return Math.round(differenceInHours);
+}
+
+function getLastFiveDays() {
+  const days = ['today', 'yesterday', 'last two days', 'last three days', 'last four days'];
+  const today = new Date();
+  const datesArray = [];
+
+  for (let i = 0; i < 5; i++) {
+      const date = new Date(today);
+      date.setDate(today.getDate() - i);
+
+      const year = date.getFullYear();
+      const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based
+      const day = String(date.getDate()).padStart(2, '0');
+
+      datesArray.push({
+          day: days[i],
+          date: `${year}-${month}-${day}`
+      });
+  }
+
+  return datesArray;
+}
 
 const Driver: NextPage = () => {
   const { user } = useUserContext();
   const router = useRouter();
+  const [timeline, setTimeline] = useState('this_week');
+  const [fixedDate, setFixedDate] = useState('');
+  const [dateStart, setDateStart] = useState('');
+  const [dateEnd, setDateEnd] = useState('')
 
   const { id } = router.query;
 
@@ -79,6 +141,9 @@ const Driver: NextPage = () => {
 
   const [currentPage, setCurrentPage] = useState(1);
   const [pageSize, setPageSize] = useState(5);
+  const lastFiveDays = getLastFiveDays();
+
+
 
   const {
     data: tripHistory,
@@ -87,6 +152,16 @@ const Driver: NextPage = () => {
     refetch: refetchTripHistory,
   } = useGetDriverTripHistoryQuery(
     { driverId: String(id), limit: pageSize, page: currentPage },
+    { skip: !id, refetchOnMountOrArgChange: true, refetchOnReconnect: true }
+  );
+
+  const {
+    data: onlineMonitorData,
+    isLoading: onlineMonitorDataLoading,
+    isError: onlineMonitorDataError,
+    refetch: refetchOnlineMonitorData,
+  } = useGetOnlineMonitorQuery(
+    { id: String(id), timeline, fixedDate, dateStart, dateEnd },
     { skip: !id, refetchOnMountOrArgChange: true, refetchOnReconnect: true }
   );
 
@@ -114,8 +189,8 @@ const Driver: NextPage = () => {
   }, [unblockError]);
 
   useEffect(() => {
-    if (data) console.log({data})
-  }, [data])
+    if (onlineMonitorData) console.log({onlineMonitorData})
+  }, [onlineMonitorData])
 
   const { userPermissions } = useUserPermissions();
   const currentPageUrl = router.query.current_page ? `currentPage=${router.query.current_page}` : '';
@@ -189,14 +264,88 @@ const Driver: NextPage = () => {
                   {
                     <div className="my-3">
                     <Card>
-                      <div className="flex justify-end py-2">
+                      <div className="flex justify-end pt-2">
                         <p><b>User Status: </b>{capitalizeAllFirstLetters(data.onlineStatus)}</p>
                         {data.onlineStatus === 'online' ? <div className="mt-1 mx-2" style={{backgroundColor: '#6BBE66', border: '1px solid #6BBE66', borderRadius: '50%', height: '2vh', width: '1vw'}}></div> : <div className="mt-1 mx-2" style={{backgroundColor: '#9A9A9A', border: '1px solid #9A9A9A', borderRadius: '50%', height: '2vh', width: '1vw'}}></div>}
                       </div>
                       {(data.onlineSwitch || data.offlineSwitch) && 
-                      <div className="flex justify-end py-2">
+                      <div className="flex justify-end pb-2">
                         {data.onlineStatus === 'online' ?  `Active since ${timeAgo(data.onlineSwitch)}` : `Last active ${timeAgo(data.offlineSwitch)}`}
                       </div>}
+                      {
+                        onlineMonitorData &&
+                        <div className="flex justify-between py-2">
+                          <div className="cursor-pointer text-xs bg-[#FFF5D8] p-2 rounded-lg" onClick={() => {
+                            setModalContent(
+                              <div className="w-[90%] sm:w-[50%] md:w-[40%] lg:w-[40%]">
+                                <Card bg="#FFF">
+                                  <div className="flex w-full justify-center items-center">
+                                    <div className="w-[5%] cursor-pointer" onClick={() => {
+                                      setModalContent(null)
+                                    }}>
+                                      <TimesIconRed />
+                                    </div>
+                                    <div className="font-bold w-[95%] flex justify-center items-center">Today's Online Activity</div>
+                                  </div>
+                                  <div className="flex flex-col overflow-y-scroll w-full h-[60vh]">
+                                    {
+                                      onlineMonitorData?.data?.trackers?.map((track: any) => (
+                                        <div className="flex flex-col mt-2 mb-2 mx-3 p-4 bg-[#F6F6F6] rounded-lg">
+                                          <div className="mx-3 flex justify-between">
+                                            <p className="mt-1 mb-1 font-bold">{track.type === 'online' ? `${formatTime(track.online_switch_time)} - ${formatTime(track.offline_switch_time)}` : `${formatTime(track.offline_switch_time)} - ${formatTime(track.online_switch_time)}`}</p>
+                                            <p className="mt-1 mb-1 font-bold text-xs">{track.type === 'online' ? `Online for ${calculateHoursBetween(track.online_switch_time, track.offline_switch_time)} hours` : `Offline for ${calculateHoursBetween(track.offline_switch_time, track.online_switch_time)} hours`}</p>
+                                          </div>
+                                        </div>
+                                      ))
+                                    }
+                                  </div>
+                                </Card>
+                              </div>
+                            )
+                          }}>View Today's Online Activity</div>
+                          <div className="cursor-pointer text-xs bg-[#FFF5D8] p-2 rounded-lg" onClick={() => {
+                            setModalContent(
+                              <div className="w-[90%] sm:w-[50%] md:w-[40%] lg:w-[40%]">
+                                <Card bg="#FFF">
+                                  <div className="flex justify-end w-full">
+                                    <div className="w-auto cursor-pointer" onClick={() => {
+                                      setModalContent(null)
+                                    }}>
+                                      <TimesIconRed />
+                                    </div>
+                                  </div>
+
+                                  <div className="w-full mt-4">
+                                    <TextField
+                                      placeholder="Search by a single date or range"
+                                      label="Search by a single date or range"
+                                      type="date"
+                                      onChange={(e) =>{
+                                          setTimeline('')
+                                          setFixedDate(e.target.value)
+                                        }
+                                      }
+                                      className="my-3"
+                                      startIcon={<SearchIcon />}
+                                    />
+                                  </div>
+
+                                    <div className="w-full mt-4">
+                                      {
+                                        lastFiveDays.map(day => (
+                                          <div className="flex flex-col mt-2 mb-2 mx-3 p-4 bg-[#F6F6F6] rounded-lg">
+                                            <p className="mt-1 mb-1 font-bold text-xs">{capitalizeFirstLetter(day?.day)}</p>
+                                          </div>
+                                        ))
+                                      }
+                                    </div>
+                                </Card>
+                              </div>
+                            )
+                          }}>View Extended Online Activity</div>
+                      </div>
+                      }
+                      
                     </Card>
                     </div>
                   }
